9350e3f3237f45ad67565d997b68b44ac9609a31
Alejandro Busi <Alejandrobusi@gmail.com>
2023-08-29 10:14:00 -0300

    first commit

---
+++ Modulo 2/Clase 3 javascript intro/app.js
@@ -0,0 +1,17 @@
+/*
+const valueOfPi = 3.14;
+console.log(valueOfPi);
+*/
+let name = 'Alejandro';
+let esMayor = false;
+let age = 29;
+let lastName = null;
+
+console.log('Hola, me llamo' + ' ' + name);
+console.log(esMayor);
+console.log(age);
+console.log(lastName);
+
+console.log(typeof lastName);
+
+const user = 'Nico';
---
+++ Modulo 2/Clase 3 js/app.js
@@ -0,0 +1,98 @@
+function test() {
+  alert('Hola');
+}
+//el comentario
+
+/*
+Comentario multi linea
+
+
+let nombre = 'Alejandro';
+let edad = 29;
+const dni = 33558774;
+// console.log('Hola, mi nombre es ' + nombre + ' y tengo ' + edad + ' años');
+console.log(
+  `Hola, mi nombre es ${nombre} y tengo ${edad} años. Mi dni es ${dni}`
+);
+
+edad = 25;
+
+console.log(
+  `Hola, mi nombre es ${nombre} y tengo ${edad} años. Mi dni es ${dni}`
+);
+
+console.error('Este es un error');
+console.warn('Esta es una advertencia');
+//Tipos de datos
+let age = 25; //number
+let name = 'Ale'; //string
+let isBigger = true; // boolean
+const dni = '2549634574';
+let isNull = null; //null
+let arrays = ['ale', 'guille', 'dani']; //array
+let persona = {
+  name: 'Ale',
+  lastName: 'Busi',
+}; //objeto
+
+console.log(name, age);
+console.log(dni);
+console.log(isBigger);
+
+console.log(2 * '2');
+
+console.log(isNull);
+console.log(arrays);
+console.log(persona);
+
+// Operadores aritmeticos
+console.log(5 + 5);
+console.log(5 - 3);
+console.log(5 * 5);
+console.log(6 / 2);
+console.log(6 % 2);
+
+//operadores unitarios
+
+let num1 = 2;
+num1++;
+console.log(num1);
+
+let num2 = 3;
+num2--;
+console.log(num2);
+
+let isLog = true;
+console.log(!isLog);
+
+if (!isLog) {
+  alert('Estamos dentro de el IF');
+}
+// operadores condicionales relacionales
+
+let edadDeDani = prompt('Ingresa tu edad');
+console.log(edadDeDani);
+// console.log(4 == '4');
+// console.log(4 != 4);
+// console.log(4 === '4');
+// console.log('15' !== 15);
+// console.log(10 > 9);
+// console.log(15 < 35);
+// console.log(edadDeDani >= 18);
+// console.log(edadDeDani <= 18);
+if (edadDeDani !== null) {
+  if (edadDeDani >= 18) {
+    alert('Podes pasar, maquina cosmica de mal');
+  } else {
+    alert('jaaaa, no podes pasar men :v');
+  }
+}
+
+*/
+// operadores logicos
+//AND
+console.log(10 > 9 && 10 < 10);
+//OR
+console.log(10 > 10 || 10 < 10);
+//NOT
+console.log(10 > 10 || 10 < 10);
